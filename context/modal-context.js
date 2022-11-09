7f13ea2908f16d2cd2d8e3f0b3558dffebee3530
hallofdave <93009866+hallofdave@users.noreply.github.com>
2022-11-09 14:34:36 -0700

    added button animation and modal context

--- context/modal-context.js
+++ context/modal-context.js
@@ -1,66 +1,66 @@
 import { createContext, useState } from "react";
 
 const modalDescriptions = [
-  "Some sample text for individual coverage.",
-  "Some sample text for family coverage.",
-  "Some sample text for small business coverage.",
-  "Some sample text for retired coverage.",
-  "Some sample text for Why Perry.",
+	"We make it simple to get individual plans to best suit your needs. Whether you're 26, getting insurance for the first time, or you have recently had a life changing event. We want to help.",
+	"Family is important to us. We can help you with finding the perfect coverage for the whole family to help give you peace of mind.",
+	"Make providing the best insurance as simple as possible. We cater to all businesses from 1 - 100 employees. We take care of your insurance so you can focus on your business.",
+	"We want to help! Your retirement is your time. Let us make insurance as simple and worry free as possible",
+	"Some sample text for Why Perry.",
 ];
 
 const ModalContext = createContext({
-  isShown: false,
-  hideModal: () => {},
-  showModal: () => {},
-  description: "",
-  title: "",
+	isShown: false,
+	hideModal: () => {},
+	showModal: () => {},
+	description: "",
+	title: "",
 });
 
 export const ModalContextProvider = (props) => {
-  const [isShown, setIsShown] = useState(false);
-  const [modalTitle, setModalTitle] = useState("");
-  const [modalDescription, setModalDescription] = useState("");
+	const [isShown, setIsShown] = useState(false);
+	const [modalTitle, setModalTitle] = useState("");
+	const [modalDescription, setModalDescription] = useState("");
 
-  const hideModalHandler = () => {
-    setIsShown(false);
-  };
+	const hideModalHandler = () => {
+		setIsShown(false);
+	};
 
-  const showModalHandler = (event) => {
-    const eventCurrentTarget = event.currentTarget.classList;
+	const showModalHandler = (event) => {
+		const eventCurrentTarget = event.currentTarget.classList;
 
-    if (eventCurrentTarget.contains("card_ind")) {
-      setModalTitle("Individuals");
-      setModalDescription(modalDescriptions[0]);
-    } else if (eventCurrentTarget.contains("card_fam")) {
-      setModalTitle("Families");
-      setModalDescription(modalDescriptions[1]);
-    } else if (eventCurrentTarget.contains("card_smb")) {
-      setModalTitle("Small Businesses");
-      setModalDescription(modalDescriptions[2]);
-    } else if (eventCurrentTarget.contains("card_ret")) {
-      setModalTitle("Retired");
-      setModalDescription(modalDescriptions[3]);
-    } else {
-      setModalTitle("Why Perry?");
-      setModalDescription(modalDescriptions[4]);
-    }
+		if (eventCurrentTarget.contains("card_ind")) {
+			setModalTitle("Individuals");
+			setModalDescription(modalDescriptions[0]);
+		} else if (eventCurrentTarget.contains("card_fam")) {
+			setModalTitle("Families");
+			setModalDescription(modalDescriptions[1]);
+		} else if (eventCurrentTarget.contains("card_smb")) {
+			setModalTitle("Small Businesses");
+			setModalDescription(modalDescriptions[2]);
+		} else if (eventCurrentTarget.contains("card_ret")) {
+			setModalTitle("Retired");
+			setModalDescription(modalDescriptions[3]);
+		} else {
+			setModalTitle("Why Perry?");
+			setModalDescription(modalDescriptions[4]);
+		}
 
-    setIsShown(true);
-  };
+		setIsShown(true);
+	};
 
-  return (
-    <ModalContext.Provider
-      value={{
-        isShown,
-        hideModal: hideModalHandler,
-        showModal: showModalHandler,
-        description: modalDescription,
-        title: modalTitle,
-      }}
-    >
-      {props.children}
-    </ModalContext.Provider>
-  );
+	return (
+		<ModalContext.Provider
+			value={{
+				isShown,
+				hideModal: hideModalHandler,
+				showModal: showModalHandler,
+				description: modalDescription,
+				title: modalTitle,
+			}}
+		>
+			{props.children}
+		</ModalContext.Provider>
+	);
 };
 
 export default ModalContext;
