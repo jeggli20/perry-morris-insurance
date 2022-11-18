de5e77ea2247f9f1fe892a5ead1c23d2e318831c
hallofdave <93009866+hallofdave@users.noreply.github.com>
2022-11-18 13:51:50 -0700

    responsive design / email / animation

--- components/Email/EmailAdmin.js
+++ components/Email/EmailAdmin.js
@@ -1,55 +1,63 @@
 import EmailStyles from "./EmailStyles";
 
 const EmailAdmin = (data) => {
-  const { name, email, provider, message, address, dob, policy } =
-    data.emailObj;
-  return (
-    <body style={EmailStyles.containerStyles}>
-      <header>
-        <h1 style={EmailStyles.titleStyles}>Perry Morris</h1>
-      </header>
-      <main style={EmailStyles.contentStyles}>
-        <h1 style={EmailStyles.headerStyles}>
-          You have received a new message from {name}
-        </h1>
-        <p style={EmailStyles.textStyles}>
-          Here is the information that {name} sent you:
-        </p>
-        <ul style={EmailStyles.infoStyles}>
-          <li>
-            Name: <span style={EmailStyles.infoItemStyles}>{name}</span>
-          </li>
-          <li>
-            Email: <span style={EmailStyles.infoItemStyles}>{email}</span>
-          </li>
-          <li>
-            Address: <span style={EmailStyles.infoItemStyles}>{address}</span>
-          </li>
-          <li>
-            Date of Birth: <span style={EmailStyles.infoItemStyles}>{dob}</span>
-          </li>
-          <li>
-            Provider: <span style={EmailStyles.infoItemStyles}>{provider}</span>
-          </li>
-          <li>
-            Policy: <span style={EmailStyles.infoItemStyles}>{policy}</span>
-          </li>
-          <li>
-            Message: <span style={EmailStyles.infoItemStyles}>{message}</span>
-          </li>
-        </ul>
-        <p style={EmailStyles.textStyles}>An email has been sent to {name}</p>
-      </main>
-      <hr style={EmailStyles.hrStyles} />
-      <footer>
-        <ul style={EmailStyles.contactStyles}>
-          <li>1480 East 3100 North</li>
-          <li>Logan, UT 84341</li>
-          <li>perry@perrymorrisinsurance.com</li>
-        </ul>
-      </footer>
-    </body>
-  );
+   const { name, email, provider, message, address, dob, policy } =
+      data.emailObj;
+   return (
+      <body style={EmailStyles.containerStyles}>
+         <header>
+            <h1 style={EmailStyles.titleStyles}>Perry Morris Insurance</h1>
+         </header>
+         <hr style={EmailStyles.hrStyles} />
+         <main style={EmailStyles.contentStyles}>
+            <h1 style={EmailStyles.headerStyles}>
+               You have received a new message from {name}
+            </h1>
+            <p style={EmailStyles.textStyles}>
+               Here is the information that {name} sent you:
+            </p>
+            <ul style={EmailStyles.infoStyles}>
+               <li>
+                  Name: <span style={EmailStyles.infoItemStyles}>{name}</span>
+               </li>
+               <li>
+                  Email: <span style={EmailStyles.infoItemStyles}>{email}</span>
+               </li>
+               <li>
+                  Address:{" "}
+                  <span style={EmailStyles.infoItemStyles}>{address}</span>
+               </li>
+               <li>
+                  Date of Birth:{" "}
+                  <span style={EmailStyles.infoItemStyles}>{dob}</span>
+               </li>
+               <li>
+                  Provider:{" "}
+                  <span style={EmailStyles.infoItemStyles}>{provider}</span>
+               </li>
+               <li>
+                  Policy:{" "}
+                  <span style={EmailStyles.infoItemStyles}>{policy}</span>
+               </li>
+               <li>
+                  Message:{" "}
+                  <span style={EmailStyles.infoItemStyles}>{message}</span>
+               </li>
+            </ul>
+            <p style={EmailStyles.textStyles}>
+               An email has been sent to {name}
+            </p>
+         </main>
+         <hr style={EmailStyles.hrStyles} />
+         <footer>
+            <ul style={EmailStyles.contactStyles}>
+               <li>1480 East 3100 North</li>
+               <li>Logan, UT 84341</li>
+               <li>perry@perrymorrisinsurance.com</li>
+            </ul>
+         </footer>
+      </body>
+   );
 };
 
 export default EmailAdmin;
--- components/Email/EmailStyles.js
+++ components/Email/EmailStyles.js
@@ -1,58 +1,86 @@
 const EmailStyles = {
-  containerStyles: {
-    fontFamily: "Roboto, sans-serif",
-    backgroundColor: "#efefef",
-    padding: "5%",
-  },
-
-  titleStyles: {
-    fontSize: "1.5rem",
-    textAlign: "center",
-    color: "black",
-  },
-
-  contentStyles: {
-    display: "flex",
-    alignItems: "center",
-  },
-
-  headerStyles: {
-    fontSize: "1.2rem",
-    margin: "3%",
-    color: "black",
-    textAlign: "center",
-  },
-
-  infoStyles: {
-    listStyleType: "none",
-  },
-
-  textStyles: {
-    fontSize: "1rem",
-    margin: "3%",
-    color: "black",
-  },
-
-  hrStyles: {
-    height: "1px",
-    backgroundColor: "lightblue",
-    clear: "both",
-    width: "96%",
-    margin: "auto",
-  },
-
-  contactStyles: {
-    // textAlign: "center",
-    padding: "3% 0",
-    lineHeight: "16px",
-    fontSize: "12px",
-    color: "#303840",
-    listStyle: "none",
-  },
-
-  infoItemStyles: {
-    color: "goldenrod",
-  },
+   containerStyles: {
+      maxWidth: "30rem",
+      margin: "auto",
+      padding: "2rem",
+      display: "block",
+      justifyContent: "center",
+      backgroundColor: "rgb(254, 254, 254)",
+      padding: "1rem",
+      maxWidth: "50rem",
+   },
+   titleStyles: {
+      fontSize: "3rem",
+      letterSpacing: ".09rem",
+      textAlign: "center",
+      color: "#4163e0",
+      textShadow: ".03em .03em #5f1a37, -.03em -.03em rgb(30, 30, 30)",
+   },
+   contentStyles: {
+      padding: "1rem",
+      display: "block",
+      alignItems: "center",
+   },
+   headerStyles: {
+      fontFamily: "Playfair Display, sans-serif",
+      fontSize: "1.2rem",
+      textShadow: "1px 1px #fff",
+      letterSpacing: ".1rem",
+      margin: "3%",
+      color: "black",
+      textAlign: "center",
+   },
+   infoStyles: {
+      fontFamily: "Playfair Display, sans-serif",
+      color: "rgb(240, 240, 240)",
+      //textShadow: "1px 1px rgb(30, 30, 30)",
+      minWidth: "20rem",
+      fontSize: "1.2rem",
+      fontWeight: "100",
+      letterSpacing: ".03rem",
+      border: "1px solid #4163e0",
+      boxShadow:
+         "inset .1em .1em .1em rgb(6, 6, 6, .1), .5em .5em .5em rgb(6, 6, 6, .2)",
+      margin: "2rem",
+      padding: "1rem",
+      listStyleType: "none",
+      backgroundColor: "rgb(180, 180, 180)",
+   },
+   textStyles: {
+      fontFamily: "Josefin Sans, sans-serif",
+      textShadow: "1px 1px rgb(255, 255, 255)",
+      textIndent: "1rem",
+      letterSpacing: ".09rem",
+      lineHeight: "1.5rem",
+      fontSize: "1.2rem",
+      margin: "3rem",
+      color: "rgb(51, 51, 51)",
+   },
+   hrStyles: {
+      height: ".05rem",
+      backgroundColor: "#4163e0",
+      //clear: "both",
+      width: "auto",
+      margin: "2rem",
+   },
+   contactStyles: {
+      fontFamily: "Playfair Display, sans-serif",
+      fontWeight: "bold",
+      // textAlign: "center",
+      padding: "1rem",
+      margin: "1.5rem",
+      lineHeight: "1.5rem",
+      letterSpacing: ".1rem",
+      fontSize: "1rem",
+      color: "#303840",
+      listStyle: "none",
+   },
+   infoItemStyles: {
+      fontSize: "1.5rem",
+      lineHeight: "2rem",
+      fontFamily: "Josefin Sans, sans-serif",
+      color: "#4163e0",
+   },
 };
 
 export default EmailStyles;
